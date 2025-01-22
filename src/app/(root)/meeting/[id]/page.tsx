27cc0fb3c14917c4e79d74e7e9b24ad813e4d950
Titya <thitya.yem.photo@gmail.com>
2025-01-22 18:44:45 -0500

    Build Meeting Setup room and enable camera and mics

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,5 +1,36 @@
-const Meeting = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room: #{params.id}</div>;
+"use client";
+
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs";
+import { StreamCallProvider, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+import { use } from "react";
+
+const Meeting = ({ params }: { params: Promise<{ id: string }> }) => {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { id } = use(params);
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCallProvider call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCallProvider>
+    </main>
+  );
 };
 
 export default Meeting;
